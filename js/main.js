8062f3438a6357c3e445eb52272648f388ae903f
NeverMind109 <vladgladkih123@gmail.com>
2021-11-14 16:37:29 +0300

    Created: added modal dialog

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-const hotelSlider = new Swiper('.hotel-slider', {
+$(document).ready(function () {
+// Настройки для слайдера
+  const hotelSlider = new Swiper('.hotel-slider', {
     // Optional parameters
     loop: true,
     speed: 200,
@@ -8,13 +10,6 @@ const hotelSlider = new Swiper('.hotel-slider', {
       prevEl: '.hotel-slider__button--prev',
     },
 });
-// var hotelSlider = new Swiper('.hotel-slider', {
-//   keyboard: {
-//     enabled: true,
-//     onlyInViewport: false,
-//   },
-// });
-
 
 const reviewsSlider = new Swiper('.reviews-slider', {
   // Optional parameters
@@ -26,14 +21,29 @@ const reviewsSlider = new Swiper('.reviews-slider', {
     prevEl: '.reviews-slider__button--prev',
   },
 });
-// var reviewsSlider = new Swiper('.reviews-slider', {
-// keyboard: {
-//   enabled: true,
-//   onlyInViewport: false,
-// },
-// });
 
-var menuButton = document.querySelector('.menu-button');
-menuButton.addEventListener('click', function () {
-  document.querySelector('.navbar-bottom').classList.toggle('navbar-bottom--visible')
-})
+// Мобильное меню
+var menuButton = $('.menu-button');
+menuButton.on('click', function () {
+  $('.navbar-bottom').toggleClass('navbar-bottom--visible');
+});
+
+// Модальные окна
+ var modalButton = $('[data-toggle=modal]');
+ var closeModalButton = $(".modal__close");
+ modalButton.on ('click', openModal);
+ closeModalButton.on ('click', closeModal);
+
+  function openModal () {
+    var targetModal = $(this).attr("data-href");
+    $(targetModal).find(".modal__overlay").addClass('modal__overlay--visible');
+    $(targetModal).find(".modal__dialog").addClass('modal__dialog--visible');
+  }
+  function closeModal (event) {
+    event.preventDefault();
+    var modalOverlay = $('.modal__overlay');
+    var modalDialog = $('.modal__dialog');
+    modalOverlay.removeClass('modal__overlay--visible');
+    modalDialog.removeClass('modal__dialog--visible');
+  }
+});
